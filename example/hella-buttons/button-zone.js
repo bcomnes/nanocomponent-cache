2aadaa7477a4cc9165751b5b41e417b8a0ff8f7f
Bret Comnes <bcomnes@gmail.com>
2018-02-11 15:51:18 -0800

    Crazy buttons

--- example/hella-buttons/button-zone.js
+++ example/hella-buttons/button-zone.js
@@ -20,11 +20,29 @@ class ButtonZone extends Nanocomponent {
       <div>
         ${c.get('button-0').render(props)}
         ${c.get('button-1').render(props, 'Button foo')}
-        ${c.get('button-2').render({disabled: true})}
+        ${c.get('button-null').render({disabled: true})}
+        ${c.get('button-2').render(props)}
         ${c.get('button-3').render(props)}
         ${c.get('button-4').render(props)}
-        ${c.get('button-5').render(props)}
-        ${c.get('button-6', {args: ['button-whatever']}).render(props, 'Button 6')}
+        ${c.get('button-5', {args: ['button-whatever']}).render(props, 'Button 6')}
+        ${c.get('button-6').render(props)}
+        ${c.get('button-7').render(props)}
+        ${c.get('button-8').render(props)}
+        ${c.get('button-9').render(props)}
+        ${c.get('button-10').render(props)}
+        ${c.get('button-11').render(props)}
+        ${c.get('button-12').render(props)}
+        ${c.get('button-13').render(props)}
+        ${c.get('button-14').render(props)}
+        ${c.get('button-15').render(props)}
+        ${c.get('button-16').render(props)}
+        ${c.get('button-17').render(props)}
+        ${c.get('button-18').render(props)}
+        ${c.get('button-19').render(props)}
+        ${c.get('button-20').render(props)}
+        ${c.get('button-21').render(props)}
+        ${c.get('button-22').render(props)}
+        ${c.get('button-23').render(props)}
       </div>
     `
   }
